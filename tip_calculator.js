a608184cb429cd4651eb1be191bbf0bad2f3895e
sebastianvarelab <svarela@miuandes.cl>
2021-05-13 12:07:08 -0400

    second commit

---
+++ tip_calculator.js
@@ -0,0 +1,15 @@
+function app (bill,tip_p){
+    return bill,tip_p
+}
+function view (bill,tip_p,tip,total){
+    return tip 
+}
+function update (bill,tip_p){
+    return bill,tip_p
+}
+tip_p=0
+bill=0
+var prompt = require ("prompt-sync")()
+var bill= prompt( "Bill Amount?")
+var tip_p = prompt ("Tip(%)?") 
+
